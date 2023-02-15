993437f9e0235e7a35d21ff1600289903ac2e7cc
2334096040@qq.com <2334096040@qq.com>
2023-02-15 21:20:52 +0800

    新建User模型 | 新增路由文件 | 新增注册功能（未完成） | 新增发送邮箱验证码功能（未完成）

--- user/models/usermodel.go
+++ user/models/usermodel.go
@@ -1,1 +1,15 @@
 package models
+
+import "time"
+
+type User struct {
+	Id        int       `xorm:"not null pk autoincr INT"`
+	Identity  string    `xorm:"VARCHAR(20)"`
+	Email     string    `xorm:"VARCHAR(20)"`
+	Password  string    `xorm:"VARCHAR(40)"`
+	Nickname  string    `xorm:"VARCHAR(30)"`
+	Status    int       `xorm:"default 0 comment('0 正常 1被禁用') INT"`
+	CreatedAt time.Time `xorm:"created"`
+	UpdatedAt time.Time `xorm:"updated"`
+	DeletedAt time.Time `xorm:"deleted"`
+}
--- user/user.go
+++ user/user.go
@@ -1,1 +1,14 @@
-package user
+package main
+
+import (
+	_ "dynamic-password/user/db"
+	"dynamic-password/user/routes"
+)
+
+func main() {
+
+	//engine := db.GetDB()
+	r := routes.Init()
+
+	r.Run()
+}
